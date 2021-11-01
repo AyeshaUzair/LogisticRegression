8317038112517b3a9afc1ffad89390249462554b
Ayesha Uzair <ayesha.shafqat92@gmail.com>
2021-11-01 19:05:12 +0100

    prediction added

--- diabetesDS_LR_streamlit.py
+++ diabetesDS_LR_streamlit.py
@@ -9,16 +9,14 @@ Created on Thu Oct 28 19:19:57 2021
 import pandas as pd
 import pandas_profiling as pp
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import mean_squared_error,mean_absolute_error, confusion_matrix, accuracy_score, classification_report
 import math
 import pickle
-
-# import pandas_profiling as pp
-# from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 import seaborn as sns
 import plotly_express as px
-import plotly.graph_objs as go
+# import plotly.graph_objs as go
 # import plotly.subplots as sp
 import streamlit as st
 import plotly.io as pio
@@ -27,34 +25,40 @@ pio.renderers.default='browser'
 
 st.title("Diabetes Dataset Modeling")
 st.write("Select filters from sidebar to view more")
-st.sidebar.title("Select a Plot")
+st.sidebar.title("Select an option:")
 dpdwn = st.sidebar.selectbox("",[
-                                "1",
-                                "2",
-                                "3",
-                                "Prediction"
+                                "Prediction",
+                                "Model Evaluation",
+                                "Correlation Heatmap",
+                                "Basic DataSet Visualizations",
+                                "Logistic Regression Plots",
                                 ])
 
 
 df = pd.read_csv("Diabetes_dataset.csv")
-# profile = pp.ProfileReport(df)
-# profile.to_file("Diabetese_dataset_EDA.html")
 print(df.info())
 df.drop_duplicates()
 
+# Change Outcome Datatype from int (1s and 0s) to Boolean (True and False)
+# print(df['Outcome'].unique())
+#df["Outcome"] = df["Outcome"].astype(bool)
 
+# Initial Dataset Pandas Profiling
+# profile = pp.ProfileReport(df)
+# profile.to_file("Diabetese_dataset_EDA.html")
+
+
+# =============================================================================
+# # Copy dataset before alteration
+# df1 = df.copy()
+# 
 # # Mean Calculations
 # mean_insulin = df['Insulin'].mean()
 # mean_glucose = df['Glucose'].mean()
 # mean_bp = df['BloodPressure'].mean()
 # mean_bmi = df['BMI'].mean()
 # mean_skinthickness = df['SkinThickness'].mean()
-
-
-# # Copy dataset before alteration
-# df1 = df.copy()
-
-
+# 
 # # Deal with zeros in Skin thickness, BMI, BloodPressure, Glucose, Insulin
 # # Convert 0s to nan and then fillna with mean
 # df1['Insulin'] = df['Insulin'].mask(df['Insulin']==0).fillna(mean_insulin)
@@ -63,19 +67,17 @@ df.drop_duplicates()
 # df1['BMI'] = df['BMI'].mask(df['BMI']==0).fillna(mean_bmi)
 # df1['SkinThickness'] = df['SkinThickness'].mask(df['SkinThickness']==0).fillna(mean_skinthickness)
 # print(df1.info())
-
-
+# 
 # # Change Outcome Datatype
 # df1["Outcome"] = df1["Outcome"].astype(bool)
-
-
+# 
 # # Check new/cleaned dataset
 # print(df1.info())
-
-
+# 
 # New Profiling Report
 # profile_df1 = pp.ProfileReport(df1)
 # profile_df1.to_file("Diabetese_dataset_cleaned.html")
+# =============================================================================
 
 
 # Variables
@@ -87,67 +89,77 @@ X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.70, random_stat
 regressor = LogisticRegression()
 regressor.fit(X_train,y_train)
 
-
 # Model parameters
-print("coef_: ", regressor.coef_)
-print("intercept: ", regressor.intercept_)
+intercept = regressor.intercept_
+coeffs = regressor.coef_
+colls = X.columns
+evals = []
+for i in range(coeffs.shape[1]):
+    evals.append([colls[i], coeffs[0][i]])
+print(evals)
+print("Coefficients: ", coeffs)
+print("Variables: ", colls)
+print("Intercept (expected mean value of Outcome when all variables are 0): ", intercept)
 
 
-# =============================================================================
-# # Correlation
-# corr = df1.corr()
-# print(corr)
-# 
-# =============================================================================
-figure22 = sns.jointplot(data = df, kind="scatter",x="DiabetesPedigreeFunction", y = "Pregnancies",hue="Outcome")
+# Visualizing Regression Model
+fig1 = sns.lmplot(x="DiabetesPedigreeFunction", y="Outcome", data=df, logistic=True, y_jitter=.03)
+fig2 = sns.lmplot(x="Age", y="Outcome", data=df, logistic=True, y_jitter=.03)
+fig3 = sns.lmplot(x="Pregnancies", y="Outcome", data=df, logistic=True, y_jitter=.03)
+fig4 = sns.lmplot(x="BMI", y="Outcome", data=df, logistic=True, y_jitter=.03)
+fig5 = sns.lmplot(x="Glucose", y="Outcome", data=df, logistic=True, y_jitter=.03)
+fig6 = sns.lmplot(x="Insulin", y="Outcome", data=df, logistic=True, y_jitter=.03)
+fig7 = sns.lmplot(x="BloodPressure", y="Outcome", data=df, logistic=True, y_jitter=.03)
 
-# =============================================================================
-# figure1 = sns.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns, cbar = True,cmap="rocket_r")
-# 
-# =============================================================================
+# Correlation
+corr = df.corr()
+print(corr)
+
+# figure22 = sns.jointplot(data = df, kind="scatter",x="DiabetesPedigreeFunction", y = "Pregnancies",hue="Outcome")
+
+# fig8 = sns.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns, cbar = True,cmap="rocket_r")
+ 
+# Findings
 finding1 = "No. of Pregnancies, Glucose levels, BMI and Age have strong correlation with the Outcome variable"
 finding2 = "No. of Pregnancies, Glucose levels, Blood Pressure have a strong correlation with Age"
 finding3 = "Age and Blood Pressure have a high correlation"
 finding4 = "BMI and Skin Thickness have a very high correlation.. so on"
 
 
-# =============================================================================
-# figure2 = px.histogram(df1,x="Outcome")
-# 
-# =============================================================================
-
-# Test
-new_x = [[4,100,70,8,16,30,0.3,40]]
-
-
-# Prediction probability
-pred1 = regressor.predict_proba(new_x) # False Case Probability | True Case Probability
-print("x1 ", pred1)
-
-# Yes/No Prediction
-pred2 = regressor.predict(new_x) 
-print("x2 ", pred2)
-
+figure2 = px.histogram(df,x="Outcome")
+figure3 = px.histogram(df, x='Pregnancies', y='Outcome')
+figure5 = px.histogram(df, x='Age', y='Outcome')
+figure6 = px.histogram(df, x='Glucose', y='Outcome')
+# Predict from Test Dataset
 y_pred = regressor.predict(X_test)
+
+# Confusion Matrix
 conf = confusion_matrix(y_test, y_pred)
+cmtx = pd.DataFrame(
+    confusion_matrix(y_test, y_pred), 
+    index=['True: Yes', 'True: No'], 
+    columns=['Predicted: Yes', 'Predicted: No']
+)
 print("confusion matrix: ", conf)
+print(cmtx)
+
+# Pickling trained model for later use
+pickle.dump(regressor,open('model.pkl','wb'))
+# model = pickle.load(open('model.pkl','rb'))
 
 TP = conf[0][0]
 FP = conf[0][1]
 FN = conf[1][0]
 TN = conf[1][1]
 
-print(conf[0][0])
-print(conf[1][0])
-print(conf[0][1])
-print(conf[1][1])
-
 accuracy = (TP+TN) / (TP + TN + FN + TN)
 print("accuracy: ", accuracy)
 
-print(accuracy_score(y_test, y_pred))
+acc_score = accuracy_score(y_test, y_pred)
+print(acc_score)
 
-print(classification_report(y_test, y_pred))
+clasf_report = classification_report(y_test, y_pred)
+print(clasf_report)
 
 
 coeff = list(regressor.coef_[0])
@@ -158,29 +170,61 @@ features['importance'] = coeff
 features.sort_values(by=['importance'], ascending=True, inplace=True)
 features['positive'] = features['importance'] > 0
 features.set_index('Features', inplace=True)
-print(features.index)
-# figure3 = features.importance.plot(kind='barh', figsize=(11, 6),color = features.positive.map({True: 'blue', False: 'red'}))
-figure3 = px.histogram(features, x="importance", y=features.index, color = features.positive.map({True: 'blue', False: 'red'}))
-
-#figure3.plt.xlabel('Importance')
+#features.importance.plot(kind='barh', figsize=(11, 6),color = features.positive.map({True: 'blue', False: 'red'}))
+#plt.xlabel('Importance')
+# figure4 = px.histogram(features, x="importance", y=features.index, color = features.positive.map({True: 'blue', False: 'red'}))
 
 
 
-if dpdwn == "1":
-    st.subheader("Histogram: Gender Analysis")
-    st.pyplot(figure22)
-    st.write(accuracy)
+if dpdwn == "Logistic Regression Plots":
+    st.subheader("Logistic Regression Plots for all Independent Variables")
+    st.pyplot(fig1)
+    st.pyplot(fig2)
+    st.pyplot(fig3)
+    st.pyplot(fig4)
+    st.pyplot(fig5)
+    st.pyplot(fig6)
+    st.pyplot(fig7)
+
         
-if dpdwn == "2":
-    st.subheader("Histogram: Age Group Analysis")
-    # st.plotly_chart(figure2)    
-    st.write(conf)
+if dpdwn == "Basic DataSet Visualizations":
+    st.subheader("Outcome Count")
+    st.plotly_chart(figure2)    
+    st.subheader("Pregnancies vs Outcome Count")
+    st.plotly_chart(figure3) 
+    st.subheader("Age vs Outcome Count")
+    st.plotly_chart(figure5) 
+    st.subheader("Glucose vs Outcome Count")
+    st.plotly_chart(figure6) 
     
-if dpdwn == "3":
-    st.subheader("Histogram: Age Group vs Gender")
-    st.plotly_chart(figure3)
-    st.write(y_pred)
     
+if dpdwn == "Correlation Heatmap":
+    st.subheader("Correlation Heatmap")
+    fig, ax = plt.subplots()
+    sns.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns, cbar = True,cmap="rocket_r", ax=ax)
+    st.write(fig)
+    st.success(finding1)
+    st.success(finding2)
+    st.success(finding3)
+    st.success(finding4)
+    
+if dpdwn == "Model Evaluation":
+    st.header("Model Evaluation")
+    st.write("**Dependent Variable:** Outcome")
+    st.write("**Independent Variables:** [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]")
+    st.subheader("Intercept")
+    st.text(intercept)
+    st.subheader("Variable Coefficients")
+    for i in evals:
+        st.text(i)
+    st.subheader("Confusion Matrix")
+    st.write(cmtx)
+    st.subheader("Accuracy Score")
+    st.write(acc_score)
+    st.subheader("Classification Report")
+    st.write(clasf_report)
+
+
 if dpdwn == "Prediction":
     st.subheader("Prediction")
     preg = st.number_input("Enter the number of pregnancies: ")
@@ -193,8 +237,9 @@ if dpdwn == "Prediction":
     age = st.number_input("Enter the age in years: ")
     
     x_test = [[preg, glucose, bp, skin_t, insulin, bmi, dpf, age]]
+    # Yes/No Prediction
     y_pred = regressor.predict(x_test)
-    
+    st.spinner()
     if st.button("Get Result"):
         if y_pred[0] == 0:
             st.subheader("Result: False")
@@ -206,5 +251,9 @@ if dpdwn == "Prediction":
             st.error("The patient seems to have diabetes.")
             st.write("The patient seems to have diabetes.")
     
-    
+        st.subheader("Prediction Probability")
+        # Prediction probability
+        pred1 = regressor.predict_proba(x_test) # False Case Probability | True Case Probability
+        st.write("False Case Probability : {}".format(pred1[0][0]))
+        st.write("True Case Probability : {}".format(pred1[0][1]))
     
